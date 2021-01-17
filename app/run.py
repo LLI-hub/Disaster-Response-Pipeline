54079f1c7f76ad0c590f15404ae5d4da6682b256
Iván Lucas <75633815+LLI-hub@users.noreply.github.com>
2021-01-17 17:28:11 +0100

    Update run.py

--- app/run.py
+++ app/run.py
@@ -30,7 +30,7 @@ engine = create_engine('sqlite:///../data/DisasterResponse.db')
 df = pd.read_sql_table('df_clean', engine)
 
 # load model
-model = joblib.load("../models/pickle_model.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
